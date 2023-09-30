9d2e5a4d8b4d24f03ab59e60253066ed14d7e5a0
Jayson Roque <90960570+roqjayson@users.noreply.github.com>
2023-09-30 16:58:08 +0800

    Update streamlit_app.py
    Corrected header's My Parent to My Parents'

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ my_fruit_list = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/da
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('My Parents\' New Healthy Dinner')
 
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
