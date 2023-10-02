1b15d7b0c2a486ab9fbe7d2346fa3dbf78d51b0c
Jayson Roque <90960570+roqjayson@users.noreply.github.com>
2023-10-02 15:39:22 +0800

    Update streamlit_app.py
    added comments for normalizing json and dataframe display that was just added.

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,3 +25,9 @@ streamlit.header("Fruityvice Fruit Advice!")
 
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
+
+# normalize a json response
+fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
+# show it as a dataframe
+streamlit.dataframe(fruityvice_normalized)
