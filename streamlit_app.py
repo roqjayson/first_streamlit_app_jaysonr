6fce9a8ed1958b5336f61d173b1f5e54a1258089
Jayson Roque <90960570+roqjayson@users.noreply.github.com>
2023-10-02 17:13:09 +0800

    Update streamlit_app.py
    Removed last prompt and added the prompt to show up after the button is clicked

--- streamlit_app.py
+++ streamlit_app.py
@@ -61,6 +61,3 @@ if streamlit.button('Add a fruit to the List'):
   back_from_function = insert_row_snowflake(add_my_fruit)
   streamlit.text(back_from_function)
 
-streamlit.write('Thank you for adding ', add_my_fruit)
-
-
