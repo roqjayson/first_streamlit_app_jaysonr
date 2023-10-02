9410131d26f33a3b77b2af8bf47c03b5b70e7b4f
Jayson Roque <90960570+roqjayson@users.noreply.github.com>
2023-10-02 16:52:24 +0800

    Update streamlit_app.py
    Reorganized code and introduced try-except to make sure that  the correct fruit is searched and to make sure that the adding of rows is not looped.

--- streamlit_app.py
+++ streamlit_app.py
@@ -24,8 +24,17 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
-fruit_choice = streamlit.text_input('What fruit would you like information about?', 'Kiwi')
-streamlit.write('The user entered', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
+
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 
@@ -40,11 +49,3 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?', 'jackfr
 streamlit.write('Thank you for adding ', add_my_fruit)
 
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
-
-# normalize a json response
-fruityvice_normalized = pd.json_normalize(fruityvice_response.json())
-
-# show it as a dataframe
-streamlit.dataframe(fruityvice_normalized)
